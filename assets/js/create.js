c596374d130ccbe39902b0c6f1289b29a3da00a7
Lidwine Carême <lidwine.careme@gmail.com>
2023-10-03 16:20:53 +0200

    réglages filtre

--- assets/js/create.js
+++ assets/js/create.js
@@ -24,6 +24,47 @@ function generateHTML(data) {
         task.appendChild(status);
         status.classList.add("status");
 
+        //sélectionner l'élément du menu déroulant pour le filtre
+        const filter = document.getElementById('filter');
+
+        //ajouter un évenement pour détecter les changements de section
+        filter.addEventListener('change', () => {
+            //récupérer la valeur de l'option sélectionner
+            const selectedValue = filter.value;
+            //cherher toutes les articles qu'on souhaite cacher/afficher
+            const articleF = document.querySelectorAll('.task');
+
+            articleF.forEach(function(article) {
+                const stat = article.querySelector('.status');
+                  
+                switch(selectedValue) {
+                    case "toDo":
+                        if(stat.innerText === 'To do') {
+                            article.style.display = 'block';
+                        } else {
+                            article.style.display = 'none';
+                        }
+                        break;
+                    case 'doing':
+                        if(stat.innerText === 'Doing') {
+                            article.style.display = 'block';
+                        } else {
+                            article.style.display = 'none';
+                        }
+                        break;
+                    case 'done':
+                        if(stat.innerText === 'Done') {
+                            article.style.display = 'block';
+                        } else {
+                            article.style.display = 'none';
+                        }
+                        break;
+                    default:
+                        break;
+                };
+            });
+        });
+
         const delItem = document.createElement("button");
         task.appendChild(delItem);
         delItem.classList.add("delItem");
