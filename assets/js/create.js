bd22e819493fcee6c6eac9467bc5a12884db53cc
Taweria <elodieali.pro@gmail.com>
2023-10-03 15:26:21 +0200

    Days remaining

--- assets/js/create.js
+++ assets/js/create.js
@@ -1,50 +1,63 @@
+import { remainingDays } from "./remainingDays.js";
+
 let tasks = document.querySelector(".tasks");
 
+//create elements
 function generateHTML(data) {
-        const task = document.createElement("article");
-        task.classList.add("task");
+    const task = document.createElement("article");
+    task.classList.add("task");
   
-        const name = document.createElement("div");
-        name.textContent = data.name;
-        task.appendChild(name);
-        name.classList.add("name");
+    const name = document.createElement("div");
+    name.textContent = data.name;
+    task.appendChild(name);
+    name.classList.add("name");
   
-        const date = document.createElement("div");
-        date.textContent = data.date;
-        task.appendChild(date);
-        date.classList.add("date");
+    const date = document.createElement("div");
+    date.textContent = data.date;
+    task.appendChild(date);
+    date.classList.add("date");
   
-        const desc = document.createElement("div");
-        desc.textContent = data.description;
-        task.appendChild(desc);
-        desc.classList.add("description");
+    const desc = document.createElement("div");
+    desc.textContent = data.description;
+    task.appendChild(desc);
+    desc.classList.add("description");
   
-        const status = document.createElement("div");
-        status.textContent = data.pending;
-        task.appendChild(status);
-        status.classList.add("status");
+    const status = document.createElement("div");
+    status.textContent = data.pending;
+    task.appendChild(status);
+    status.classList.add("status");
 
-        const delItem = document.createElement("button");
-        task.appendChild(delItem);
-        delItem.classList.add("delItem");
+    const delItem = document.createElement("button");
+    task.appendChild(delItem);
+    delItem.classList.add("delItem");
 
-        const delItemImg = document.createElement("img");
-        delItemImg.src = "./assets/img/poubelle.png";
-        delItem.appendChild(delItemImg);
-        delItemImg.classList.add("delItemImg");
+    const delItemImg = document.createElement("img");
+    delItemImg.src = "./assets/img/poubelle.png";
+    delItem.appendChild(delItemImg);
+    delItemImg.classList.add("delItemImg");
 
-        const modifyItem = document.createElement("button");
-        task.appendChild(modifyItem);
-        modifyItem.classList.add("modifyItem");
+    const modifyItem = document.createElement("button");
+    task.appendChild(modifyItem);
+    modifyItem.classList.add("modifyItem");
 
-        const modifyItemImg = document.createElement("img");
-        modifyItemImg.src = "./assets/img/crayon-de-couleur.png";
-        modifyItem.appendChild(modifyItemImg);
-        modifyItemImg.classList.add("modifyItemImg");
+    const modifyItemImg = document.createElement("img");
+    modifyItemImg.src = "./assets/img/crayon-de-couleur.png";
+    modifyItem.appendChild(modifyItemImg);
+    modifyItemImg.classList.add("modifyItemImg");
 
-        tasks.appendChild(task);
+    const remainingDays = document.createElement("div");
+    task.appendChild(remainingDays);
+    remainingDays.classList.add("remainingDays");
+
+    tasks.appendChild(task);
+
+
+   
+    
 }
 
+
+// reset form after push add
 function resetForm() {
     document.getElementById("name").value = "";
     document.getElementById("date").value = "";
@@ -54,7 +67,7 @@ function resetForm() {
 
 const form = document.getElementById("form")
 
-currentData = {};
+let currentData = {};
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -63,7 +76,18 @@ form.addEventListener("submit", (e) => {
         currentData[value[0]] = value[1]
     }
 
-    console.log(currentData);
     generateHTML(currentData);
+
+    let days = document.querySelectorAll(".date");
+    console.log(days);
+    
+    days.forEach(date => {
+        
+        if (date.innerText != '' ) {
+            // remainingDays(date);
+            date.parentNode.querySelector('.remainingDays').innerText = remainingDays(date.innerText);
+        }
+    });
+
     resetForm();
 })
--- assets/js/delete.js
+++ assets/js/delete.js
@@ -5,7 +5,7 @@
 //ajouter son contenu -> img de poubelle
 delItem.textContent= "./assets/img/poubelle.png";
 
-//attacher un gestionnaire d'événements au boutton de suppression
+// //attacher un gestionnaire d'événements au boutton de suppression
 delItem.addEventListener('click', () => {
     //supprimer la div entière
     task.remove();
