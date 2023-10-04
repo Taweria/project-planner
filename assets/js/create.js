ad466e357b99ad1a38cc01f8dbb04f0ce0d26417
Taweria <elodieali.pro@gmail.com>
2023-10-04 15:45:01 +0200

    Style

--- assets/js/create.js
+++ assets/js/create.js
@@ -8,27 +8,57 @@ let tasks = document.querySelector(".tasks");
 //create elements
 export function generateHTML(data) {
     const task = document.createElement("article");
-    task.classList.add("task");
+    task.classList.add("task","w-96", "flex", "flex-col", "bg-white", "relative", "rounded-md");
+
+    const status = document.createElement("div");
+    status.textContent = data.pending;
+    task.appendChild(status);
+    status.classList.add("status", "w-12", "bg-darkPink", "text-center", "font-bold");
+
+    const modifyItem = document.createElement("button");
+    task.appendChild(modifyItem);
+    modifyItem.classList.add("modifyItem");
+
+    const modifyItemImg = document.createElement("img");
+    modifyItemImg.src = "./assets/img/crayon-de-couleur.png";
+    modifyItem.appendChild(modifyItemImg);
+    modifyItemImg.classList.add("modifyItemImg", "w-6", "absolute", "top-0.5", "right-8");
+
+    const delItem = document.createElement("button");
+    task.appendChild(delItem);
+    delItem.classList.add("delItem");
+
+    const delItemImg = document.createElement("img");
+    delItemImg.src = "./assets/img/poubelle.png";
+    delItem.appendChild(delItemImg);
+    delItemImg.classList.add("delItemImg", "w-6", "absolute", "top-0.5", "right-0");
+
+    //attacher un gestionnaire d'événement au bouton de supression
+    delItem.addEventListener('click', () => {
+        //supprimer la div entière
+        removeFromLocalStorage(task);
+        task.remove();
+    });
+
   
     const name = document.createElement("div");
     name.textContent = data.name;
     task.appendChild(name);
-    name.classList.add("name");
-  
-    const date = document.createElement("div");
-    date.textContent = data.date;
-    task.appendChild(date);
-    date.classList.add("date");
-  
+    name.classList.add("name", "my-5");
+
     const desc = document.createElement("div");
     desc.textContent = data.description;
     task.appendChild(desc);
     desc.classList.add("description");
   
-        const status = document.createElement("div");
-        status.textContent = data.pending;
-        task.appendChild(status);
-        status.classList.add("status");
+    const date = document.createElement("div");
+    date.textContent = data.date;
+    task.appendChild(date);
+    date.classList.add("date", "my-3");
+  
+    
+  
+        
 
         //sélectionner l'élément du menu déroulant pour le filtre
         const filter = document.getElementById('filter');
@@ -72,31 +102,9 @@ export function generateHTML(data) {
         });
 
 
-    const delItem = document.createElement("button");
-    task.appendChild(delItem);
-    delItem.classList.add("delItem");
+    
 
-    const delItemImg = document.createElement("img");
-    delItemImg.src = "./assets/img/poubelle.png";
-    delItem.appendChild(delItemImg);
-    delItemImg.classList.add("delItemImg");
-
-    //attacher un gestionnaire d'événement au bouton de supression
-    delItem.addEventListener('click', () => {
-        //supprimer la div entière
-        removeFromLocalStorage(task);
-        task.remove();
-    });
-
-
-    const modifyItem = document.createElement("button");
-    task.appendChild(modifyItem);
-    modifyItem.classList.add("modifyItem");
-
-    const modifyItemImg = document.createElement("img");
-    modifyItemImg.src = "./assets/img/crayon-de-couleur.png";
-    modifyItem.appendChild(modifyItemImg);
-    modifyItemImg.classList.add("modifyItemImg");
+    
 
 
     // allow user to modify cards
@@ -153,7 +161,7 @@ export function generateHTML(data) {
     const leftDays = document.createElement("div");
     leftDays.textContent = remainingDays(data.date);
     task.appendChild(leftDays);
-    leftDays.classList.add("remainingDays");
+    leftDays.classList.add("remainingDays", "absolute", "bottom-0", "right-0", "bg-darkPink", "px-2", "rounded");
 
     tasks.appendChild(task);
     
