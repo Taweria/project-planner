d97375edbff7c4edf7455be2610d31ee6a50fd07
Virginie Dourson <virginiedourson@gmail.com>
2023-10-04 13:52:33 +0200

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     './index.html'
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'purple': '#141E46',
+      'darkPink': '#C70039',
+      'mediumPink': '#FF6969',
+      'lightPink': '#FFF5E0',
+    },
     extend: {},
   },
   plugins: [],
